4de9297ed2c6931284e67f859b3ce4079d90a6fe
Denison <denisondealmeidaprtela@hotmail.com>
2020-08-23 03:00:15 -0300

    subindo primeira parte da calculadora para a ana estilizar

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,52 +5,126 @@ const options = {
 }
 
 
-var dadosAPI = []
-
-//Conectando com a AIP  e fazendo função para acesssar o dados DAS MOEDAS
-fetch('https://economia.awesomeapi.com.br/all', options)
-    .then(response => {
-        if(!response.ok) throw new Error('Erro ao executar requisição')
-        console.log(response.ok)
-        return response.json();
-    })
-    .then(data => {
-        escrever(data)
-
-    })
-    .catch(function (error) {
-        console.log(error);
-    })
-
-
-    function escrever (dados){
-
-        dadosAPI = []
-        nomesDasMoedas = []
-
-        //Convertendo o objeto em array
-        dadosAPI = Object.values(dados)
-
-          console.log(dadosAPI)
-
-          meusDaodosParaUso(dadosAPI)
-
-          
-
-        //para usar os dados da API  devemos usar uma function com o nome 
-        //meusDadosParaUso
-
-        // e usar assim :
-
-        // function meusDaodosParaUso(dados){
-        //     console.log(dados)
-        // }
-    }
+    renderizarInfoDasMoedas('nome_moeda_1', 'porcentagem_moeda_1', 'preco_moeda_1','USD',"Dólar Comercial")
+    renderizarInfoDasMoedas('nome_moeda_2', 'porcentagem_moeda_2', 'preco_moeda_2','BTC',"Bitcoin")
+    renderizarInfoDasMoedas('nome_moeda_3', 'porcentagem_moeda_3', 'preco_moeda_3','EUR',"Euro")
+    
+    // renderizarInfoDasMoedas('GBP');
+    // renderizarInfoDasMoedas('EUR');
 
     //chamando a função para renderizar os graficos das moedas.
     rederizarGraficosMoedas('dolar','USD')
-    rederizarGraficosMoedas('libra','GBP')
+    rederizarGraficosMoedas('libra','BTC')
     rederizarGraficosMoedas('euro','EUR')
+
+
+
+
+        function renderizarInfoDasMoedas(nome, porcentagem, preco, moeda,tipoDaMoeda){
+            var dadosAPI = []
+
+            //Conectando com a AIP  e fazendo função para acesssar o dados DAS MOEDAS
+            fetch('https://economia.awesomeapi.com.br/all', options)
+                .then(response => {
+                    if(!response.ok) throw new Error('Erro ao executar requisição')
+                    console.log(response.ok)
+                    return response.json();
+                })
+                .then(data => {
+                    escrever(data)
+
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+
+
+                function escrever (dados){
+
+                    dadosAPI = []
+                    nomesDasMoedas = []
+                    
+
+                    //Convertendo o objeto em array
+                    dadosAPI = Object.values(dados)
+
+                    dadosAPI.forEach(element => {
+                        if(element.code == moeda){
+                            nomesDasMoedas.push(element)
+                        }
+
+                    });
+
+                    //Caso exista duas moedas com código igual, o códigop ira pegar o tipo que a função
+                    //passará, e usara como padrão
+                    if(nomesDasMoedas.length > 1){
+                       
+                        nomesDasMoedas.forEach(element => {
+                            if(element.name == tipoDaMoeda){
+                                nomesDasMoedas = element
+                                
+                            }
+                        });
+                            
+                    }else{
+                        nomesDasMoedas = nomesDasMoedas[0]
+                    }
+
+                    console.log(nomesDasMoedas)
+
+                    //pegando elemento na HTML para setar conforme a moeda
+
+                    let area_nome = document.getElementById(nome)
+                    let area_porcentagem = document.getElementById(porcentagem)
+                    let area_preco = document.getElementById(preco)
+
+                    let result_color_and_sinal
+
+                    setCorAndSinal()
+                    
+                    //função para determinar a cor (vermelho ou verde) e o sinal (+ ou -), 
+                    //dependendo da variação da moeda 
+                    function setCorAndSinal() {
+                        let nume_em_porcent = parseFloat(nomesDasMoedas.pctChange)
+                        
+                        console.log("porcentagem" + nume_em_porcent + nomesDasMoedas.pctChange)
+                        if(nume_em_porcent < 0){
+                            let sinal = "-"
+                            area_porcentagem.setAttribute("style","background-color:red")
+                            result_color_and_sinal = nomesDasMoedas.pctChange + "%"
+                        }else{
+                            let sinal = "+"
+                            area_porcentagem.setAttribute("style","background-color:green")
+                            result_color_and_sinal = sinal + nomesDasMoedas.pctChange + "%"
+                        }
+
+                    }
+
+                    //escrevendo os resultados no HTML
+                    area_nome.innerHTML = nomesDasMoedas.code + " - " + tipoDaMoeda
+                    area_porcentagem.innerHTML = result_color_and_sinal
+                    area_preco.innerHTML = "R$ " + nomesDasMoedas.bid
+
+                    //"+" + nomesDasMoedas.pctChange + "%"
+                    meusDaodosParaUso(dadosAPI)
+
+                    nomesDasMoedas = ""
+
+                    
+
+                    
+
+                    //para usar os dados da API  devemos usar uma function com o nome 
+                    //meusDadosParaUso
+
+                    // e usar assim :
+
+                    // function meusDaodosParaUso(dados){
+                    //     console.log(dados)
+                    // }
+                }
+
+        }
     
 
             //função que consume a API das moedas pegando a variação e o fechamento em 10 dias, e montando o gráfico no 
@@ -98,8 +172,8 @@ fetch('https://economia.awesomeapi.com.br/all', options)
                                 label: "",
                                 data: bidDosDias,
                                 borderWidth: 3,
-                                borderColor: '#DB3E07',
-                                backgroundColor: '',
+                                borderColor: '#00A78B',
+                                backgroundColor: '#282828',
                             },
                             ]
                             },
@@ -145,15 +219,12 @@ fetch('https://economia.awesomeapi.com.br/all', options)
             datasBOVESP = []
             dadosBOVESP = []
             BOVESP = Object.values(date)
-            console.log(BOVESP[0])
 
             BOVESP[0].forEach(element => {
                 datasBOVESP.push(element.data)
                 dadosBOVESP.push(element.fechamento)
             });
 
-            console.log(datasBOVESP)
-            console.log(dadosBOVESP)
 
             renderizarGrágicoBOOVESP(dadosBOVESP, datasBOVESP)
 
@@ -171,8 +242,8 @@ fetch('https://economia.awesomeapi.com.br/all', options)
                         label: "Euro",
                         data: dados,
                         borderWidth: 3,
-                        borderColor: '#3B393B',
-                        backgroundColor: '',
+                        borderColor: '#034138',
+                        backgroundColor: '#99c5bf',
                     },
                     ]
                     },
